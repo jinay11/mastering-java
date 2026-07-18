836b1b84ff514f61e05f1c7bc8f42132baa202f6
Jinay Madhavi <jinaymadhavi74@gmail.com>
2026-07-18 15:04:05 +0530

    overridin same method name and same parameters into diffrent class

---
+++ src/oops/methodOverriging.java
@@ -0,0 +1,39 @@
+package oops;
+
+class Bank1 {
+
+	public void rateOfInterest() {
+		System.out.println("Generally Rate of Interest is 10%");
+	}
+
+}
+
+class SBI extends Bank1 {
+
+	public void rateOfInterest() {
+		System.out.println("SBI Rate of Interest is 7%");
+	}
+
+}
+
+class HDFC extends Bank1 {
+
+	public void rateOfInterest() {
+		System.out.println("HDFC Rate of Interest is 8%");
+	}
+}
+
+// method overriding means same method name and same parameters with different class
+
+public class methodOverriging {
+	public static void main(String[] args) {
+		Bank1 b = new Bank1();
+		b.rateOfInterest();
+
+		SBI s = new SBI();
+		s.rateOfInterest();
+
+		HDFC h = new HDFC();
+		h.rateOfInterest();
+	}
+}
