892861795ef2199c8e1cdaee8eacf0517ef0d10f
Jinay Madhavi <jinaymadhavi74@gmail.com>
2026-09-11 20:03:25 +0530

    variable

---
+++ src/basic/Variable.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class Variable {
+    public static void main(String[] args) {
+        int age = 28;
+        String name = "Jon Doe";
+        double salary = 50000.50;
+
+        System.out.println("Welcome " + name + " you are a " + age + " years old ");
+        System.out.println("Salary : " + salary);
+
+    }
+}
