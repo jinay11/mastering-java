1e85237065e3881144405730b654e63baadde5c0
Jinay Madhavi <jinaymadhavi74@gmail.com>
2026-09-11 20:24:26 +0530

    variable and scope

--- src/basic/Variable.java
+++ src/basic/Variable.java
@@ -1,6 +1,14 @@
 package basic;
 
 public class Variable {
+
+    static String  sentence = "This is the instance variable ";
+
+    public static void show() {
+        int num = 12;
+        System.out.println( num+ " this number access only this method");
+    }
+
     public static void main(String[] args) {
         int age = 28;
         String name = "Jon Doe";
@@ -8,6 +16,17 @@ public class Variable {
 
         System.out.println("Welcome " + name + " you are a " + age + " years old ");
         System.out.println("Salary : " + salary);
+
+        show();
+
+//        System.out.println(num); local variable not accessiable
+        System.out.println(sentence);
+
+        if(true) {
+            int x = 10;
+            System.out.println(x);
+        }
+//        System.out.println(x); not access
 
     }
 }
