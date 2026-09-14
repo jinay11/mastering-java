243c799a90c5bfb701a070dbe16eb9dd3f25407e
Jinay Madhavi <jinaymadhavi74@gmail.com>
2026-09-14 16:50:38 +0530

    operator practies

---
+++ src/basic/OperatorsPrc.java
@@ -0,0 +1,83 @@
+package basic;
+
+public class OperatorsPrc {
+    public static void main(String[] args) {
+
+//        System.out.println("Arithmetic Operators");
+//        int a = 10,  b = 3;
+
+        //Addition
+//        int add = a + b;
+        //substraction
+//        int sub = a - b;
+        //multiplication
+//        int multi = a * b;
+        //division
+//        int div = a/b;
+        //modules
+//        int mod = a %b;
+
+//        System.out.println("Sum : " + add);
+//        System.out.println("Difference : " + sub);
+//        System.out.println("multiplication : " + multi);
+//        System.out.println("division : " + div);
+//        System.out.println("Modules : " + mod);
+
+//        System.out.println("Unary Operator");
+//        int a = 10, b = 10;
+//        System.out.println("Post Increment " + (a++));
+//        System.out.println("Pre Increment " + (++a));
+
+//        System.out.println("Post Decrement " + (b--));
+//        System.out.println("Pre Decrement : " + (--b));
+
+//        System.out.println("Assignment operator");
+
+        //intial value
+//        int num = 10;
+//        System.out.println("Intial value : " + num);
+
+        //add 5 -> num = num + 5;
+//        num += 5;
+//        System.out.println("After +5 : " + num);
+
+        //multiply by 2 -> num = num * 2
+//        num *=2 ;
+//        System.out.println("After *2 : " + num);
+
+        //sustract 5 -> num = num -5;
+//        num -= 5;
+//        System.out.println("After -5 : "+num);
+
+        //divide by 2 -> num = num / 2
+//        num /= 2;
+//        System.out.println("After /2 : " + num);
+
+        // remainder after dividing by 3 → num = num % 3
+//        num %= 3;
+//        System.out.println("After %3: " + num);
+
+//        System.out.println("Relational operator ");
+//        int a = 10;
+//        int b = 3;
+//        int c = 10;
+//        System.out.println(" a > b : " + (a >b));
+//        System.out.println("a < b : " + (a < b));
+//        System.out.println("a >= c : " + (a >= c));
+//        System.out.println("a <= b : " + (a <= b ));
+//        System.out.println("a == b : " + (a == b));
+//        System.out.println("a != b : " + (a != b));
+
+//        System.out.println("Relational operators ");
+//        boolean x = true;
+//        boolean y = false;
+//        System.out.println("X && y : " + (x && y));
+//        System.out.println("x || y : " + (x || y));
+//        System.out.println("!x : " + (!x));
+
+        System.out.println("Ternary Operator");
+        int age = 1;
+        String result = (age >= 18) ? "Go to the party " : "you can't go to the party";
+        System.out.println(result);
+    }
+}
