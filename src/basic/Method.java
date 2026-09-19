9bef5ea40c93774df2f6e4f81df1aee38317558a
Jinay Madhavi <jinaymadhavi74@gmail.com>
2026-09-19 15:47:32 +0530

    Method in java

---
+++ src/basic/Method.java
@@ -0,0 +1,42 @@
+package basic;
+
+public class Method {
+
+    //static method
+    public static void greet() {
+        System.out.println("Hello Java Devloper :) ");
+    }
+
+    //instance method
+    public void printMessage() {
+        System.out.println("Hello from method");
+    }
+
+    //method call stack
+    public void methodA() {
+        methodB();
+    }
+
+    public void methodB() {
+        methodC();
+    }
+
+    public void methodC() {
+        System.out.println("Hello from method A, B and C From method call stack");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Method in java");
+
+        Method m = new Method();
+
+        m.printMessage();
+
+        m.methodA();
+
+        //static method access  directly
+        greet();
+
+    }
+}
