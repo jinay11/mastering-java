ce326b8dc3d9a1b916d31127a8090bc4fdb336e5
Jinay Madhavi <jinaymadhavi74@gmail.com>
2026-09-20 00:14:50 +0530

    access modifiers and some change in first.java

---
+++ src/basic/AccessModifiers.java
@@ -0,0 +1,30 @@
+package basic;
+
+public class AccessModifiers {
+
+    //private modifiers
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        System.out.println("Welcome " + name);
+        return name;
+    }
+
+    //Default class
+    String language;
+
+    public static void main(String[] args) {
+        System.out.println("Access Modifiers");
+
+        AccessModifiers am = new AccessModifiers();
+        am.setName("Jon");
+        am.getName();
+
+        am.language = "Java";
+        System.out.println(am.language);
+    }
+}
--- src/basic/first.java
+++ src/basic/first.java
@@ -2,6 +2,8 @@ package basic;
 
 public class first {
     public static void main(String[] args) {
+
         System.out.println("This is my first project in java ");
+
     }
 }
