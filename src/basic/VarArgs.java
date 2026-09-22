06a6234d4a1bdaa377c2844540f67ac565934928
Jinay Madhavi <jinaymadhavi74@gmail.com>
2026-09-22 19:20:51 +0530

    variable arguments in java

---
+++ src/basic/VarArgs.java
@@ -0,0 +1,23 @@
+package basic;
+
+public class VarArgs {
+
+    public void display(int... numbers) {
+//        System.out.println(numbers.length);
+        for(int i : numbers) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Variable arguments : ");
+
+        VarArgs var = new VarArgs();
+        var.display(10);
+        var.display(20,30);
+        var.display(40,50,60);
+        var.display(70,80,90,100);
+
+    }
+}
